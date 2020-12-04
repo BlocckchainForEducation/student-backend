a6da54c72e44a573b041d8f645668272a81f0984
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-04 10:24:18 +0700

    :pencil2: update master-router

--- routes/student/student-router.js
+++ routes/student/student-router.js
@@ -10,4 +10,10 @@ router.use(sawtoothAccsRouter);
 const fetchEncryptData = require("./fetch-encrypt-data");
 router.use(fetchEncryptData);
 
+const decryptData = require("./decrypt-data");
+router.use(decryptData);
+
+const genToken = require("./gen-token");
+router.use(genToken);
+
 module.exports = router;
