3fbef8feb9693e58b0c8fbd065199e6ac8ac0a23
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-04 10:23:23 +0700

    :pencil2: fetch-encrypt-data: add error handler

--- routes/student/fetch-encrypt-data.js
+++ routes/student/fetch-encrypt-data.js
@@ -5,9 +5,13 @@ const { ROLE } = require("../user-mng/role");
 const { fetchEncryptDataOfAccount } = require("./sawtooth-cli");
 
 router.get("/encrypted-data", authen, author(ROLE.STUDENT), async (req, res) => {
-  const publicKey = req.publicKey;
-  const encryptData = await fetchEncryptDataOfAccount(publicKey);
-  res.json(encryptData);
+  try {
+    const publicKey = req.publicKey;
+    const encryptData = await fetchEncryptDataOfAccount(publicKey);
+    res.json(encryptData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
