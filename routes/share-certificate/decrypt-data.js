963f0f9841cfff59272661b203de6e23562ec636
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-21 22:12:53 +0700

    :pencil2: decrpyt-data: add pub65 too

--- routes/share-certificate/decrypt-data.js
+++ routes/share-certificate/decrypt-data.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { authen } = require("../acc/protect-middleware");
 const ecies = require("ecies-geth");
+const secp256k1 = require("secp256k1");
 
 router.post("/decrypt-data", authen, async (req, res) => {
   try {
     const privateKeyHex = req.body.privateKeyHex;
+    const publicKeyHex65 = Buffer.from(secp256k1.publicKeyCreate(Buffer.from(privateKeyHex, "hex"), false)).toString("hex");
     const encryptData = req.body.encryptData;
     if (!privateKeyHex || !encryptData) return res.status(400).json("bad request, check body: privateKeyHex, encrpytData");
     const certificate = await decryptCert(privateKeyHex, encryptData.certificate);
     const subjects = await decryptSubjects(privateKeyHex, encryptData.subjects);
-    res.json({ publicKeyHex: encryptData.publicKeyHex, certificate, subjects });
+    res.json({ publicKeyHex: encryptData.publicKeyHex, publicKeyHex65, certificate, subjects });
   } catch (error) {
     console.log(error);
     res.status(500).json(error.toString());
